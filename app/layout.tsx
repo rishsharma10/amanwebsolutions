bc0637bd61bac29eabb90875329f041bf13dd036
rishabhsharma <rishsha147@gmail.com>
2026-07-24 08:43:55 +0530

    fix: update Google Analytics script strategy to afterInteractive for improved tracking reliability

--- app/layout.tsx
+++ app/layout.tsx
@@ -104,9 +104,9 @@ export default function RootLayout({
         {/* Google Analytics */}
         <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-NFKQYZX8BB"
-          strategy="lazyOnload"
+          strategy="afterInteractive"
         />
-        <Script id="google-analytics" strategy="lazyOnload">
+        <Script id="google-analytics" strategy="afterInteractive">
           {`
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
